6431a2264ac261024d6917c741c484ce08c910aa
Simon - fantomitechno <simon@renoux.dev>
2024-03-31 15:59:28 +0200

    🚧 Display skin when creating the /give

--- astro.config.ts
+++ astro.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from 'astro/config';
-
 import node from "@astrojs/node";
 import image from './build/image.js';
+import vue from "@astrojs/vue";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://simon.renoux.dev",
   output: "server",
   adapter: node({
     mode: "standalone"
   }),
-  integrations: [image()],
+  integrations: [image(), vue()],
   scopedStyleStrategy: "where"
 });
