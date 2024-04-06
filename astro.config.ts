3c5e36396b4b0b559f5e739a141d7216ba2b8f02
Simon - fantomitechno <simon@renoux.dev>
2024-04-06 20:23:22 +0200

    🔥 So long Vue

--- astro.config.ts
+++ astro.config.ts
@@ -1,15 +1,14 @@
 import { defineConfig } from 'astro/config';
 import node from "@astrojs/node";
 import image from './build/image.js';
-import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://simon.renoux.dev",
+  site: "https://mc.renoux.dev",
   output: "server",
   adapter: node({
     mode: "standalone"
   }),
-  integrations: [image(), vue()],
+  integrations: [image()],
   scopedStyleStrategy: "where"
 });
